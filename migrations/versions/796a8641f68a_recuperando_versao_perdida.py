7e26988ec757f0a2e3c9b87cabda8be32a990704
pedro-cruzz <pedrocruzvb123@gmail.com>
2026-01-16 10:42:25 -0300

    feat: implementação final

---
+++ migrations/versions/796a8641f68a_recuperando_versao_perdida.py
@@ -0,0 +1,24 @@
+"""Recuperando versao perdida
+
+Revision ID: 796a8641f68a
+Revises: 
+Create Date: 2025-12-26 10:47:34.236918
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '796a8641f68a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
